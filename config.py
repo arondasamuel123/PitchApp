3de564f0835a230b712ac4f4150e1e362f47e26e
arondasamuel123 <arondasamuel123@gmail.com>
2020-02-08 14:43:43 +0300

    Created test files for the tables and model file

--- config.py
+++ config.py
@@ -10,6 +10,11 @@ class ProdConfig(Config):
     Production config child class
     '''
 
+class TestConfig(Config):
+    '''
+    Test config child class
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://user:123456@localhost/pitch_test'
 
 class DevConfig(Config):
     DEBUG = True
--- serve.py
+++ serve.py
@@ -5,6 +5,12 @@ app = create_app('development')
 manager = Manager(app)
 manager.add_command('serve',Server)
 
+@manager.command
+def test():
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+    
 
 
 if __name__== '__main__':
